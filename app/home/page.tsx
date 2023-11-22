7d514f9e4940de4351918305b5475edb9cd8f6b9
Rafael Gomes <silveiragomesr@gmail.com>
2023-11-22 20:39:07 -0300

    feat: implementa versao inicial da pagina home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,33 @@
+'use client'
+import { useState } from 'react'
+import Stack from '@mui/material/Stack'
+import Appbar from "@mui/material/AppBar"
+import Box from "@mui/material/Box"
+import { LocalStorage } from '@/utils/LocalStorage'
+import { TableSolicitacoes } from '@/components/TableSolicitacoes'
+
 
 export default function Home () {
-    return (
-      <div>PAGINA HOME</div>
-    )
-  }
-  
+
+  const [solicitacoes, setSolicitacoes] = useState([{ id: 1, nome: 'Nome', cpf: '123121212', telefone: '2899992399', status: 'Ativo' }])
+
+  return (
+    <>
+      <Appbar style={{ height: '60px' }}>
+        <Stack direction="row" justifyContent="space-between" alignItems="center" paddingX="10px" height="100%">
+          <Box>Desafio Readi Fullstack</Box>
+
+          <Box>
+            {LocalStorage.getUserEmail()}
+          </Box>
+        </Stack>
+      </Appbar>
+
+      <Stack paddingTop="100px" alignItems="center" >
+        <Box minWidth="80vw">
+          <TableSolicitacoes solicitacoes={solicitacoes} />
+        </Box>
+      </Stack>
+    </>
+  )
+}
---
+++ components/TableSolicitacoes.tsx
@@ -0,0 +1,68 @@
+'use client'
+import { useState } from 'react'
+import Stack from '@mui/material/Stack'
+
+import { DataGrid, GridColDef, } from '@mui/x-data-grid'
+import Button from '@mui/material/Button'
+import Alert from '@mui/material/Alert'
+
+import { SolicitacaoService } from '@/services/SolicitacaoService'
+
+interface SolicitacoesProps {
+    solicitacoes: any[]
+}
+
+const TableSolicitacoes = (props: SolicitacoesProps) => {
+    const [showFeedbackAlert, setShowFeedbackAlert] = useState(false)
+    const [feedbackMessage, setFeedbackMessage] = useState('')
+
+    async function deleteSolicitacao (solicitacaoId: string) {
+        const solicitacaoResponse = await SolicitacaoService.delete(solicitacaoId)
+        setShowFeedbackAlert(true)
+        setFeedbackMessage(solicitacaoResponse.data.message)
+
+        setTimeout(() => {
+            setShowFeedbackAlert(false)
+            setFeedbackMessage('')
+        }, 3000)
+    }
+
+    const dataTableColumns: GridColDef[] = [
+        { field: 'id', headerName: 'ID' },
+        { field: 'nome', headerName: 'Nome' },
+        { field: 'cpf', headerName: 'CPF' },
+        { field: 'telefone', headerName: 'Telefone' },
+        { field: 'status', headerName: 'Status' },
+        {
+            field: 'actions', headerName: 'Actions', minWidth: 200, renderCell: (item) => {
+                return (
+                    <Stack direction="row" width="100%" spacing={2}>
+                        <Button color='error' variant='contained' onClick={() => deleteSolicitacao(item.row.id)}>
+                            Deletar
+                        </Button>
+                    </Stack>
+                )
+            }
+        },
+    ]
+
+
+    return (
+        <>
+            {showFeedbackAlert &&
+                <div style={{ position: 'absolute', top: '80px', right: '20px' }}>
+                    <Alert severity='error' color='error' >
+                        {feedbackMessage}
+                    </Alert>
+                </div>
+            }
+
+            <DataGrid autoHeight
+                rows={props.solicitacoes}
+                columns={dataTableColumns}
+            />
+        </>
+    )
+}
+
+export { TableSolicitacoes }
