05dc3fb2d3fa7a9e1a3a727bdff3a1045ce41b33
Rafael Gomes <silveiragomesr@gmail.com>
2023-11-22 20:54:45 -0300

    feat: adiciona função de logoff

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,15 +1,25 @@
 'use client'
 import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { MdLogout } from 'react-icons/md'
 import Stack from '@mui/material/Stack'
 import Appbar from "@mui/material/AppBar"
 import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
+
 import { LocalStorage } from '@/utils/LocalStorage'
 import { TableSolicitacoes } from '@/components/TableSolicitacoes'
 
 
 export default function Home () {
+  const router = useRouter()
 
   const [solicitacoes, setSolicitacoes] = useState([{ id: 1, nome: 'Nome', cpf: '123121212', telefone: '2899992399', status: 'Ativo' }])
+
+  function logoff () {
+    LocalStorage.clearUserData()
+    router.push('/')
+  }
 
   return (
     <>
@@ -17,16 +27,14 @@ export default function Home () {
         <Stack direction="row" justifyContent="space-between" alignItems="center" paddingX="10px" height="100%">
           <Box>Desafio Readi Fullstack</Box>
 
-          <Box>
-            {LocalStorage.getUserEmail()}
-          </Box>
+          <Button variant='contained' color='error' onClick={() => logoff()}>
+            <Box marginRight={1}>Desconectar</Box> <MdLogout />
+          </Button>
         </Stack>
       </Appbar>
 
       <Stack paddingTop="100px" alignItems="center" >
-        <Box minWidth="80vw">
-          <TableSolicitacoes solicitacoes={solicitacoes} />
-        </Box>
+        <TableSolicitacoes solicitacoes={solicitacoes} />
       </Stack>
     </>
   )
